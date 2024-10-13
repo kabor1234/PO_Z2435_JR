35931618d0291a4dc4dca55db5cee0104d50490a
Jakub <278003@student.pwr.edu.pl>
2024-10-13 09:39:44 +0200

    podstawowe działania 123

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1;
+        int number2;
+
+        System.out.println("Podaj piewsza liczbe: ");
+        number1 = scanner.nextInt();
+        System.out.println("Podaj druga liczbe: ");
+        number2 = scanner.nextInt();
+
+        System.out.println("Dodawanie: " + (number1+number2));
+        System.out.println("Odejmowanie: " + (number1-number2));
+        System.out.println("Mnozenie: " + (number1*number2));
+        System.out.println("Dzielenie: " + (number1/number2));
+    }
+}
