623fa54d40abd8395feee7f6e1e6c19b9a75bbcd
Jakub <278003@student.pwr.edu.pl>
2024-11-26 20:29:10 +0100

    utworzenie klas dla szablonu MVVM, rozpoczęcie pracy nad możliwością zalogowania konta

---
+++ projekt_magazyn/projekt_magazyn/CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projekt_magazyn.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        public static readonly DependencyProperty PasswordProperty;
+
+        public SecureString Password
+        {
+            get {  return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ projekt_magazyn/projekt_magazyn/ViewModel/LoginViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace projekt_magazyn.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _inViewVisible = true;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+                OnPropertyChanged("Username");
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+                OnPropertyChanged("Password");
+            }
+
+        }
+        public string ErrorMessage
+        {
+            get
+            {
+                return _errorMessage;
+            }
+
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
+
+        }
+        public bool InViewVisible
+        {
+            get
+            {
+                return _inViewVisible;
+            }
+
+            set
+            {
+                _inViewVisible = value;
+                OnPropertyChanged("ErrorMessage");
+            }
+
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand PasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+
+
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length < 3 || Password == null || Password.Length < 3)
+                validData = false;
+            else 
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
