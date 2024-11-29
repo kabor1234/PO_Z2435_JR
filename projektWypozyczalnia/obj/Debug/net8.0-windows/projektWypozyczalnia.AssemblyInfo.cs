f7bfde81e435f024d32cb7af5faebe6c40d0c7b1
Jakub <278003@student.pwr.edu.pl>
2024-11-29 21:42:28 +0100

    fullscreen

--- projektWypozyczalnia/obj/Debug/net8.0-windows/projektWypozyczalnia.AssemblyInfo.cs
+++ projektWypozyczalnia/obj/Debug/net8.0-windows/projektWypozyczalnia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projektWypozyczalnia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c75a7d9e7f9897d27e590506f7e660b8a6068a29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71b311e3e1698a61d65a8ba0804c8aee28db6bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("projektWypozyczalnia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projektWypozyczalnia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
