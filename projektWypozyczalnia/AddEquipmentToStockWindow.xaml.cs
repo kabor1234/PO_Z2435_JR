b747716e0ecfc5ef1ffca32cc2183fb3a44b4c6f
Jakub <278003@student.pwr.edu.pl>
2025-01-07 19:11:28 +0100

    przygotowanie okienek osprzętu do dodania logiki dodawania osprzętu do bazy, zmiana nazw kolumn w datagrid z angielskich na polskie

--- projektWypozyczalnia/AddEquipmentToStockWindow.xaml.cs
+++ projektWypozyczalnia/AddEquipmentToStockWindow.xaml.cs
@@ -8,4 +8,20 @@ public partial class AddEquipmentToStockWindow : Window
     {
         InitializeComponent();
     }
+
+    private void AddNewEquipmentItem_OnClick(object sender, RoutedEventArgs e)
+    {
+        AddNewEquipmentItem addNewEquipmentItem = new AddNewEquipmentItem();
+        addNewEquipmentItem.Show();
+    }
+
+    private void Cancel_OnClick(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void AddEquipmentToStock_onClick(object sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 }
